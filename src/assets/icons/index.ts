0b911440f471419a37f7c6fa4d23102143f4916a
piarovira <piarovira98@gmail.com>
2025-09-25 23:11:27 -0300

    STYLING | created theme and added assets

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,8 @@
+export { default as Plus } from "./plus.svg";
+export { default as Db } from "./db.svg";
+export { default as Graph } from "./graph.svg";
+export { default as Settings } from "./settings.svg";
+export { default as ArrowDown } from "./arrow-down.svg";
+export { default as Search } from "./search.svg";
+export { default as Edit } from "./edit.svg";
+export { default as Delete } from "./delete.svg";
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      fontSize: {
+        xs: ["12px", { lineHeight: "20px" }],
+        sm: ["13px", { lineHeight: "20px" }],
+        base: ["14px", { lineHeight: "20px" }],
+        lg: ["30px", { lineHeight: "36px" }],
+      },
+      colors: {
+        brand: "#ADABFF",
+        font: {
+          primary: "#09090B",
+          secondary: "#565656",
+          tertiary: "#828282",
+          quaternary: "#868686",
+          light: "#808593",
+        },
+        border: {
+          black: "#09090B14",
+          primary: "#ECEDEF",
+          deepBlack: "#00000029",
+        },
+        button: {
+          bg: "#09090B0A",
+        },
+      },
+      fontWeight: {
+        normal: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+      },
+      boxShadow: {
+        xs: "0px 1px 2px 0px #1018280D",
+        sm: "0px 1px 3px 0px #1018281A",
+      },
+    },
   },
   plugins: [],
-}
-
+};
